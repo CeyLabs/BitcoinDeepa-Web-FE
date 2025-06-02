cf504e024e5c66e3aaf1b20f90f4da87b6b9ef45
Kasun Chanuka Fernando <chanukaf7@gmail.com>
2025-06-02 12:21:12 +0530

    feat: add Header and Footer components to layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,21 +1,24 @@
-import type React from "react"
-import type { Metadata } from "next/dist/lib/metadata/types/metadata-interface"
-import { Exo_2 } from "next/font/google"
-import "./globals.css"
-import { ThemeProvider } from "@/src/components/theme-provider"
+import type React from "react";
+import type { Metadata } from "next/dist/lib/metadata/types/metadata-interface";
+import { Exo_2 } from "next/font/google";
+import "./globals.css";
+import { ThemeProvider } from "@/src/components/theme-provider";
+import Navbar from "@/src/components/navbar";
+import Footer from "@/src/components/footer";
 
 const exo2 = Exo_2({
   subsets: ["latin"],
   weight: ["300", "400", "500", "600", "700"],
   variable: "--font-exo2",
-})
+});
 
 export const metadata: Metadata = {
   title: "BitcoinDeepa",
-  description: "Bitcoin Deepa — Where innovation, Bitcoin, and community build together in Sri Lanka 🇱🇰",
-    generator: 'Next.js 15.3.0',
-  applicationName: 'BitcoinDeepa',
-  referrer: 'origin-when-cross-origin',
+  description:
+    "Bitcoin Deepa — Where innovation, Bitcoin, and community build together in Sri Lanka 🇱🇰",
+  generator: "Next.js 15.3.0",
+  applicationName: "BitcoinDeepa",
+  referrer: "origin-when-cross-origin",
   keywords: [
     "Bitcoin",
     "Sri Lanka",
@@ -24,24 +27,29 @@ export const metadata: Metadata = {
     "Bitcoin News",
     "Bitcoin Events",
     "Bitcoin Resources",
-    "Bitcoin Wallets",],
-}
+    "Bitcoin Wallets",
+  ],
+};
 
 export default function RootLayout({
   children,
 }: Readonly<{
-  children: React.ReactNode
+  children: React.ReactNode;
 }>) {
   return (
     <html lang="en" suppressHydrationWarning>
       <body className={`${exo2.className} antialiased`}>
-        <ThemeProvider attribute="class" defaultTheme="dark" enableSystem={false} disableTransitionOnChange>
+        <ThemeProvider
+          attribute="class"
+          defaultTheme="dark"
+          enableSystem={false}
+          disableTransitionOnChange
+        >
+          <Navbar />
           {children}
+          <Footer />
         </ThemeProvider>
       </body>
     </html>
-  )
+  );
 }
-
-
-import './globals.css'
