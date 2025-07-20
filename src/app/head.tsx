429caebabcc084610c31aef02dfc00625d9fac2c
Nisal Chandrasekara <cmnisal@gmail.com>
2025-07-20 09:36:21 +0530

    Update metadata with book info

---
+++ src/app/head.tsx
@@ -0,0 +1,11 @@
+export default function Head() {
+  return (
+    <>
+      <meta name="title" content="බිට්කොයින් පෙළහර - Book Launch & Purchase" />
+      <meta
+        name="description"
+        content="Official Sinhala translation of 'The Bullish Case for Bitcoin' is now launched! Buy the book online and get it delivered to your doorstep in Sri Lanka. Limited copies available."
+      />
+    </>
+  );
+}
